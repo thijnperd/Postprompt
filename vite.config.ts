ceeda09ba431d376b3b30f11d09d4a2124d722cc
@thijnperd <thijnfigo@gmail.com>
2026-05-08 12:51:22 +0200

    Update base path to match repo name Postprompt

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/PostPrompt/",
+  base: "/Postprompt/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
